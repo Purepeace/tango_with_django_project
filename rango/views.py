d116325a8d7163b2e97a6185fe1d9df5bd8933a6
Povilas Plukas <2270620p@student.gla.ac.uk>
2018-01-25 21:18:37 +0000

    commit chapter 4
    Signed-off-by: Povilas Plukas <2270620p@student.gla.ac.uk>

--- rango/views.py
+++ rango/views.py
@@ -4,13 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse(
-    """Rango says hey there partner! <br/> 
-    <a href='/rango/about/'>About</a>
-    """)
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+    # return HttpResponse(
+    # """Rango says hey there partner! <br/>
+    # <a href='/rango/about/'>About</a>
+    # """)
 
 def about(request):
-    return HttpResponse(
-    """Rango says here is the about page. <br/>
-    <a href="/rango/">Index</a>
-    """)
+    context_dict = {'boldmessage': "This tutorial has been put together by Povilas"}
+    return render(request, 'rango/about.html', context=context_dict)
+
+    # return HttpResponse(
+    # """Rango says here is the about page. <br/>
+    # <a href="/rango/">Index</a>
+    # """)
