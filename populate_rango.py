1af6278fb63ab17ac56031634c5ddebfc519ffe1
Povilas Plukas <2270620p@student.gla.ac.uk>
2018-01-26 16:24:43 +0000

    commit 2 chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -9,27 +9,35 @@ def populate():
     python_pages = [
         {"views": 128, "likes": 64},
         {"title": "Official Python Tutorial",
-         "url": "http://docs.python.org/2/tutorial/"},
+         "url": "http://docs.python.org/2/tutorial/",
+         "views": 69},
         {"title": "How to Think like a Computer Scientist",
-         "url": "http://www.greenteapress.com/thinkpython/"},
+         "url": "http://www.greenteapress.com/thinkpython/",
+         "views": 420},
         {"title": "Learn Python in 10 Minutes",
-         "url": "http://www.korokithakis.net/tutorials/python/"}]
+         "url": "http://www.korokithakis.net/tutorials/python/",
+         "views": 1488}]
 
     django_pages = [
         {"views": 64, "likes": 32},
         {"title": "Official Django Tutorial",
-         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "views": 666},
         {"title": "Django Rocks",
-         "url": "http://www.djangorocks.com/"},
+         "url": "http://www.djangorocks.com/",
+         "views": 636},
         {"title": "How to Tango with Django",
-         "url": "http://www.tangowithdjango.com/"}]
+         "url": "http://www.tangowithdjango.com/",
+         "views": 42}]
 
     other_pages = [
         {"views": 32, "likes": 16},
         {"title": "Bottle",
-         "url": "http://bottlepy.org/docs/dev/"},
+         "url": "http://bottlepy.org/docs/dev/",
+         "views": 7},
         {"title": "Flask",
-         "url": "http://flask.pocoo.org"}]
+         "url": "http://flask.pocoo.org",
+         "views": 123}]
 
     cats = {"Python": {"pages": python_pages},
             "Django": {"pages": django_pages},
@@ -38,7 +46,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["pages"][0]["views"], cat_data["pages"][0]["likes"])
         for p in cat_data["pages"][1:]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/admin.py
+++ rango/admin.py
@@ -3,11 +3,13 @@ from rango.models import Category, Page
 # Register your models here.
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'url']
+    list_display = ['title', 'category', 'url', 'views']
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     fields = ['likes', 'views', 'name']
-# admin.register(Category, CategoryAdmin)
+
+
